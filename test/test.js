1dad61002a1d7f2a13a7a77ed61ddb7c3978cfa4
Frank Roth <frankred@web.de>
2015-11-12 12:59:41 +0100

    Test image added

--- test/test.js
+++ test/test.js
@@ -3,7 +3,21 @@ var debug = require('debug')('ocr-by-image-url:test');
 var ocr = require('../index');
 
 describe('ocr-by-image-url', function () {
-    it('Get image by url and run ocr on it', function (done) {
+
+    it('image0', function (done) {
+        ocr.getImageText('https://raw.githubusercontent.com/frankred/node-ocr-by-image-url/master/test/resources/image0.png', null, function (error, text) {
+
+            if (error) {
+                throw error;
+            }
+
+            debug('detected text: ', text);
+            assert.equal('Hello tesseract', text.trim());
+            done();
+        });
+    });
+
+    it('image1', function (done) {
         ocr.getImageText('https://raw.githubusercontent.com/frankred/node-ocr-by-image-url/master/test/resources/image1.jpg', null, function (error, text) {
 
             if (error) {
@@ -11,6 +25,8 @@ describe('ocr-by-image-url', function () {
             }
 
             debug('detected text: ', text);
+            assert.equal('LOREM IPSUM\nDOLOR SIT AMET!', text.trim());
+
             done();
         });
     });
